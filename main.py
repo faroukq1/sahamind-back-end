7190b25ec0caad5603ea4e10994bf92b86f81e4d
faroukq1 <faroukq1@gmail.com>
2025-12-31 18:08:42 +0100

    feat: make some comments about how to access the server via local network

--- main.py
+++ main.py
@@ -16,3 +16,10 @@ def hello () :
 app.include_router(auth.router)
 app.include_router(journal.router)
 
+# uvicorn main:app --host 0.0.0.0 --port 8000
+# this command to make it accessable by IP adress
+
+
+# adi ay t3i nti ak windows user
+# you can just run this command in ubuntu to get ip4v address
+# hostname -I
